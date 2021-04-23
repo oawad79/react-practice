6f0ea6e2636d5d3507dcbb0858b72fc068ee9018
Osama <oawad79@gmail.com>
2021-04-23 10:22:13 -0500

    more

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 import initialDataSet from "./data/initialDetails";
 import SearchPeople from "./components/SearchPeople";
-import Sidebar from "./components/Sidebar";
-import Navbar from "./components/NavBar";
+import NavBar from "./components/NavBar";
 
 
 function App() {
-  const [menuVisible, setMenuVisible] = useState(false);
-
-  const menuOn = () => {
-      setMenuVisible(!menuVisible);
-  }
-
   return (
       <>
-        <Navbar menuClicked={menuOn}/>
-        <Sidebar isVisible={menuVisible}>
-            <h1>Cool Scfi movies</h1>
-            <ul>
-                <li>
-                    <a href="https://www.imdb.com/title/tt0816692/">Interstellar</a>
-                </li>
-                <li>
-                    <a href="https://www.imdb.com/title/tt0083658/">Blade Runner</a>
-                </li>
-                <li>
-                    <a href="https://www.imdb.com/title/tt0062622/">
-                        2001: a space odyssey
-                    </a>
-                </li>
-            </ul>
-        </Sidebar>
-        <SearchPeople people={initialDataSet}/>
-
+         <NavBar />
+         <SearchPeople people={initialDataSet}/>
       </>
   );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,7 +5,7 @@ type Props = {
 }
 
 function SearchBar(props: Props) {
-    return <div className="text-center py-4">
+    return <div className="text-center py-20">
         <label htmlFor="first_field" className="font-bold text-lg py-2 px-3">Search</label>
         <input name="first_field" className="border-2 border-black py-2 px-2 text-grey-darkest" type="text" onChange={props.onChange}/>
     </div>;
